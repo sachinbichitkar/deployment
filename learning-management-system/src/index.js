b3a13c7594e954c0dc4c701895acb9500f0738a9
DESKTOP-L4UNPUL\sachi <sachinbichitkar09@gmail.com>
2024-12-16 01:09:37 +0530

    Created LMS Project

--- learning-management-system/src/index.js
+++ learning-management-system/src/index.js
@@ -6,12 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter basename="/deployment">
+root.render( 
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </BrowserRouter>
+  </React.StrictMode> 
 );
 
 // If you want to start measuring performance in your app, pass a function
